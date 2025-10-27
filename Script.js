116c57a1db4fdae244a795c46d53364f95335479
EstherOluwayemisi <abiodunesther77@gmail.com>
2025-10-27 09:17:09 -0700

    update:added clse-icon

--- Script.js
+++ Script.js
@@ -1,6 +1,13 @@
 const hamburger = document.getElementById('hamburger');
-  const navLinks = document.getElementById('nav-links');
+const closeIcon = document.getElementById('close-icon');
+const navLinks = document.getElementById('nav-links');
 
-  hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-  });
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+
+  if (navLinks.classList.contains('active')) {
+    closeIcon.src = 'Asset/close-icon.webp';
+  } else {
+    closeIcon.src = 'Asset/hamburger.svg';
+  }
+});
